c477460a330fc7cc0b846b8ada33c675d78ed038
Maria Anna Tsapali <100848733+mariannatsapali@users.noreply.github.com>
2022-07-23 11:41:07 +0300

    Clone of homepad-of-pechlivanidis Project

---
+++ components/Layout.js
@@ -0,0 +1,169 @@
+import Head from 'next/head';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+
+import { createTheme } from '@mui/material/styles';
+
+import React, { useContext, useState } from 'react';
+import classes from '../utils/classes';
+import Cookies from 'js-cookie';
+import { useRouter } from 'next/router';
+import NextLink from 'next/link';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Container,
+  Link,
+  Switch,
+  Badge,
+  Button,
+  Menu,
+  MenuItem,
+  Box,
+} from '@mui/material';
+
+import { Store } from '../utils/Store';
+
+export default function Layout({ title, description, children }) {
+  const { state, dispatch } = useContext(Store);
+  const { darkMode, cart, userInfo } = state;
+
+  const theme = createTheme({
+    components: {
+      MuiLink: {
+        defaultProps: {
+          underline: 'hover',
+        },
+      },
+    },
+
+    typography: {
+      h1: {
+        fontSize: '1.6rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+      h2: {
+        fontSize: '1.4rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+    },
+    palette: {
+      mode: darkMode ? 'dark' : 'light',
+      primary: {
+        main: '#f0c000',
+      },
+      secondary: {
+        main: '#208080',
+      },
+    },
+  });
+
+  const router = useRouter();
+
+  const darkModeChangeHandler = () => {
+    dispatch({ type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON' });
+    const newDarkMode = !darkMode;
+    Cookies.set('darkMode', newDarkMode ? 'ON' : 'OFF');
+  };
+  const [anchorEl, setAnchorEl] = useState(null);
+  const loginClickHandler = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+  const loginMenuCloseHandler = (e, redirect) => {
+    setAnchorEl(null);
+    if (redirect) {
+      router.push(redirect);
+    }
+  };
+  const logoutClickHandler = () => {
+    setAnchorEl(null);
+    dispatch({ type: 'USER_LOGOUT' });
+    Cookies.remove('userInfo');
+    Cookies.remove('cartItems');
+    Cookies.remove('shippinhAddress');
+    Cookies.remove('paymentMethod');
+    router.push('/');
+  };
+
+  return (
+    <Box>
+      <Head>
+        <title>{title ? `${title} - Next Amazona` : 'Next Amazona'}</title>
+        {description && <meta name="description" content={description}></meta>}
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static" sx={classes.appbar}>
+          <Toolbar>
+            <NextLink href="/" passHref>
+              <Link>
+                <Typography sx={classes.brand}>
+                  Homepads of Pechlivanidis
+                </Typography>
+              </Link>
+            </NextLink>
+            <Box sx={classes.grow}></Box>
+            <Box>
+              <Switch
+                checked={darkMode}
+                onChange={darkModeChangeHandler}
+              ></Switch>
+              <NextLink href="/cart" passHref>
+                <Link>
+                  {cart.cartItems.length > 0 ? (
+                    <Badge
+                      color="secondary"
+                      badgeContent={cart.cartItems.length}
+                    >
+                      Cart
+                    </Badge>
+                  ) : (
+                    'Cart'
+                  )}
+                </Link>
+              </NextLink>
+              {userInfo ? (
+                <>
+                  <Button
+                    aria-controls="simple-menu"
+                    aria-haspopup="true"
+                    onClick={loginClickHandler}
+                    sx={classes.navbarButton}
+                  >
+                    {userInfo.name}
+                  </Button>
+                  <Menu
+                    id="simple-menu"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    onClose={loginMenuCloseHandler}
+                  >
+                    <MenuItem onClick={loginMenuCloseHandler}>Profile</MenuItem>
+                    <MenuItem onClick={loginMenuCloseHandler}>
+                      My account
+                    </MenuItem>
+                    <MenuItem onClick={logoutClickHandler}>Logout</MenuItem>
+                  </Menu>
+                </>
+              ) : (
+                <NextLink href="/login" passHref>
+                  <Link>Login</Link>
+                </NextLink>
+              )}
+            </Box>
+          </Toolbar>
+        </AppBar>
+        <Container sx={classes.main}>{children}</Container>
+        <Box component="footer" sx={classes.footer}>
+          <Typography>
+            Please contact: <b>homepadsp@gmail.com</b> for sales
+          </Typography>
+          <Typography>All rights reserved. Next Amazona.</Typography>
+        </Box>
+      </ThemeProvider>
+    </Box>
+  );
+}
